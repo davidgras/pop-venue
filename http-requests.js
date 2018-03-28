50fed9aecaa1068d0e180d8336f2046151a40cb1
David Gras <davidgrasreyes@gmail.com>
2018-03-28 14:41:38 +0100

    Adding HTTP Requests module to execute GET calls.

---
+++ http-requests.js
@@ -0,0 +1,34 @@
+/**
+ * HTTP Requests module
+ */
+
+// Libraries
+var request = require('request');
+
+/**
+ * Generic GET call
+ * - url (String): Complete protocol+host+path URI.
+ * - params (Array): Get parameters to be converted as a query string.
+ * - callback (function): Callback receiving response body as parameter.
+ *
+ * It throws an exception if request error occurs.
+ */
+exports.get = (url,params,callback) => {
+        request.get(url+'?'+buildQueryString(params),(err,res,body) => {
+        if( err ) {
+            console.log(err)
+            throw new Error(err);
+        }
+        callback(body);
+    })
+}
+
+// Build a query string from parameters array
+function buildQueryString(params) {
+    if(params) {
+        var query = Object.keys(params)
+            .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+            .join('&');
+        return query;
+    }
+}
