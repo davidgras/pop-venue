54d663fe5744c8e88732dc517fd0894f748b6678
David Gras <davidgrasreyes@gmail.com>
2018-03-28 17:34:56 +0100

    Replacing search input by GoogleMaps autocomplete field.

--- public/popvenues.js
+++ public/popvenues.js
@@ -2,19 +2,37 @@
  * PopVenues jQuery functions
  */
 
-$(() => {
+// Location autocomplete field
+var autocomplete;
 
-    // Search location
-    $('#search').click(function(){
-        var $input = $('#location');
-        var $output = $('#result');
-        if( $input ) {
-            $.getJSON('ws/venues/'+$input.val(), (data)=>{
-                console.log(data);
-                $output.html( JSON.stringify(data) );
-            });
-        }
+// Initialize google maps components
+function init() {
+    // LOCATION AUTOCOMPLETE
+    // Create the autocomplete object, restricting the search to geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        (document.getElementById('location')),
+        {types: ['geocode']});
+    // When the user selects an address from the dropdown, calculate distance and point-of-interest.
+    autocomplete.addListener('place_changed', searchLocation);
+}
+
+// Search for popular venues
+function searchLocation() {
+
+    // Validate location coordinates
+    if( !autocomplete || !autocomplete.getPlace() || !autocomplete.getPlace().geometry || !autocomplete.getPlace().geometry.location ) {
+        return;
+    }
+    
+    // Get lat/lon from geometry
+    var lat = autocomplete.getPlace().geometry.location.lat();
+    var lon = autocomplete.getPlace().geometry.location.lng();
+    
+    // WS request
+    var $output = $('#result');
+    $.getJSON(`ws/venues/${lat}/${lon}`, (data)=>{
+        // Print output
+        $output.html( JSON.stringify(data) );
     });
-    
-    
-});
+}
+
